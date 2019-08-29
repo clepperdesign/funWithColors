bab0e4a15911be3caa356bea1ec874a1f330ab79
Clepper Design <33584689+clepperdesign@users.noreply.github.com>
2019-08-29 09:42:35 -0700

    updated views.detail

--- views.py
+++ views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from .models import Color
-# Create your views here.
-#def index(request):
-    #return HttpResponse("<h1>Hello world </h1>")
+from .models import Color, ColorForm
+
 def detail(request, colorname):
-    return HttpResponse('<body bgcolor=%s>' % colorname)
+    bodybg = colorname
+    colorslist =  Color.objects.all()
+    context = {
+        'bodybg' : bodybg,
+        'colorslist' : colorslist,
+    }
+    template = loader.get_template('colortime/detail.html')
+    return HttpResponse(template.render(context, request))
+    #return HttpResponse('<body bgcolor=%s>' % colorname)
 def index(request):
     colorslist =  Color.objects.all()
     template = loader.get_template('colortime/index.html')
@@ -14,4 +20,3 @@ def index(request):
         'colorslist': colorslist,
     }
     return HttpResponse(template.render(context, request))
-
